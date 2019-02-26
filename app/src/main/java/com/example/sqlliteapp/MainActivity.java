7d5a3c0f1bb9ad7128a817236e0a0639f47b0a5a
Isaias <isgori2@hotmail.com>
2019-02-26 00:33:32 -0500

    Picasso implementation and test

--- app/src/main/java/com/example/sqlliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqlliteapp/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.sqlliteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageUser);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 }
